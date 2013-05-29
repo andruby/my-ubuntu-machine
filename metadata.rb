eca769c7924a7f8fb46103f0dd08bb47c7fb6ce3
Andrew Fecheyr <andrew@bedesign.be>
2013-05-29 17:10:54 +0200

    Blank cookbook skeleton.
    Created with: berks cookbook my-ubuntu-machine --maintainer="Andrew Fecheyr" --maintainer-email="andrew@bedesign.be"

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "my-ubuntu-machine"
+maintainer       "Andrew Fecheyr"
+maintainer_email "andrew@bedesign.be"
+license          "All rights reserved"
+description      "Installs/Configures my-ubuntu-machine"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: my-ubuntu-machine
+# Recipe:: default
+#
+# Copyright (C) 2013 Andrew Fecheyr
+# 
+# All rights reserved - Do Not Redistribute
+#
